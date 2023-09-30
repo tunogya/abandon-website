8fb61accc7a93248f8216e2b2d40017a7d337d85
tunogya <20251942+tunogya@users.noreply.github.com>
2023-09-30 14:11:51 +0800

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,22 +9,22 @@ export default function Home(): JSX.Element {
       description="Welcome to AbandonAI.">
       <div className="flex flex-col justify-center items-center w-full">
         <div
-          className={"flex flex-col w-full text-center items-center justify-center text-white h-[600px] rounded-b-[88px] "}
+          className={"flex flex-col w-full text-center items-center justify-center text-white h-[600px] md:rounded-b-[88px] "}
           style={{
             backgroundImage: `url("/img/desktop-background.svg")`,
             backgroundSize: "cover",
             backgroundPosition: 'center',
           }}
         >
-          <p className={"text-6xl font-bold"}>Pay as you go</p>
-          <p className={'mt-4 text-xl'}>
+          <p className={"text-4xl md:text-6xl font-bold"}>Pay as you go</p>
+          <p className={'mt-4 text-lg md:text-xl'}>
             Simple and flexible. Only pay for what you use.
           </p>
-          <div className={'flex justify-center mt-8 gap-2'}>
+          <div className={'flex flex-col md:flex-row justify-center my-8 gap-2 w-full md:w-fit px-4'}>
             <a
               href={"https://app.abandon.ai/chat"}
               target={'_blank'}
-              className={'bg-white text-black px-4 py-2 rounded font-bold cursor-pointer flex items-center gap-2'}
+              className={'bg-white text-black px-4 py-2 rounded font-bold cursor-pointer flex items-center gap-2 justify-between'}
             >
               ChatGPT
               <ArrowRightIcon className={'w-5 h-5'} />
@@ -32,7 +32,7 @@ export default function Home(): JSX.Element {
             <a
               href={"https://app.abandon.ai/"}
               target={'_blank'}
-              className={'bg-white text-black px-4 py-2 rounded font-bold cursor-pointer flex items-center gap-2'}
+              className={'bg-white text-black px-4 py-2 rounded font-bold cursor-pointer flex items-center gap-2 justify-between'}
             >
               Completion
               <ArrowRightIcon className={'w-5 h-5'} />
@@ -40,7 +40,7 @@ export default function Home(): JSX.Element {
             <a
               href={"https://app.abandon.ai/"}
               target={'_blank'}
-              className={'bg-white text-black px-4 py-2 rounded font-bold cursor-pointer flex items-center gap-2'}
+              className={'bg-white text-black px-4 py-2 rounded font-bold cursor-pointer flex items-center gap-2 justify-between'}
             >
               DALL·E
               <ArrowRightIcon className={'w-5 h-5'} />
@@ -49,30 +49,31 @@ export default function Home(): JSX.Element {
         </div>
 
         <div className={"mt-20 md:mt-40 px-4"}>
-          <div className={"text-[36px] font-bold"}>
+          <div className={"text-3xl md:text-[36px] font-bold text-center"}>
             "Abandon yourself to a life of pleasure!"
           </div>
         </div>
 
         <div className={'p-4 w-full mt-20 md:mt-40 px-4 md:px-20 xl:px-40'}>
-          <div className={"bg-blue-800 px-10 py-8 rounded-2xl text-[32px] text-white"}>
+          <div className={"bg-blue-800 px-10 py-8 rounded-2xl text-[16px] md:text-[32px] text-white"}>
             <p className={"font-bold"}>
               ChatGPT: get instant answers, find creative inspiration,<br/> and learn something new.
             </p>
-            <p className={'text-[16px] mt-6'}>
+            <p className={'text-[12px] md:text-[16px] mt-6'}>
               GPT-3.5 Turbo models are capable and cost-effective. gpt-3.5-turbo is the flagship model of this family and is optimized for dialog.
             </p>
-            <p className={'text-[16px]'}>
+            <br/>
+            <p className={'text-[12px] md:text-[16px]'}>
               4K Content Input: $0.0015 / 1K tokens
             </p>
-            <p className={'text-[16px]'}>
+            <p className={'text-[12px] md:text-[16px]'}>
               4K Content Output: $0.002 / 1K tokens
             </p>
-            <p className={'text-[16px]'}>
+            <p className={'text-[12px] md:text-[16px]'}>
               + Service Fees 30%
             </p>
             <a href={'https://openai.com/chatgpt'} target={'_blank'}>
-              <p className={'text-[16px] underline mt-6'}>
+              <p className={'text-[12px] md:text-[16px] underline mt-6'}>
                 Learn more about ChatGPT
               </p>
             </a>
@@ -80,10 +81,10 @@ export default function Home(): JSX.Element {
         </div>
 
         <div className={'mt-20 md:mt-40 text-center'}>
-          <div className={"text-[36px] font-bold"}>
+          <div className={"text-3xl md:text-[36px] font-bold"}>
             Partners
           </div>
-          <div className={'w-full flex items-center justify-center gap-10 mt-10'}>
+          <div className={'w-full flex flex-col md:flex-row items-center justify-center gap-10 mt-10'}>
             <a href={'https://openai.com'} target={'_blank'}>
               <img src={"/img/openai-logo.svg"} alt={'OpenAI'} className={'w-24 h-10'}/>
             </a>
@@ -102,8 +103,6 @@ export default function Home(): JSX.Element {
           </div>
         </div>
 
-
-
         <div
           className={"w-full mt-20 md:mt-40"}
         >
@@ -114,9 +113,9 @@ export default function Home(): JSX.Element {
             }}
           />
           <div
-            className={"bg-[#c8dfff] w-full py-16 text-center flex flex-col justify-center items-center gap-4"}
+            className={"bg-[#c8dfff] w-full py-16 text-center flex flex-col justify-center items-center gap-4 px-4"}
           >
-            <p className={"text-[40px] font-bold text-[#081B4B]"}>Let's go</p>
+            <p className={"text-3xl md:text-[40px] font-bold text-[#081B4B]"}>Let's go</p>
             <p className={"text-md text-[#4D5B7C]"}>Sign up now and you'll be using GPT-3.5 and GPT-4 on AbandonAI in just minutes.</p>
             <a
               href={'https://app.abandon.ai/'}
