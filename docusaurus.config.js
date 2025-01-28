bdfb795a1190937aecefca10811f08f8ecfaae38
tunogya <tunogya@qq.com>
2025-01-28 19:12:31 +0800

    use Courier Prime

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -105,7 +105,7 @@ const config = {
   ],
   
   stylesheets: [
-    'https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap',
+    'https://fonts.googleapis.com/css2?family=Courier+Prime:ital,wght@0,400;0,700;1,400;1,700&display=swap',
   ]
 };
 
